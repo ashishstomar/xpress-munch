cb1e9f02ee804011b28a1ce9510c5b88eba68647
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-05-31 22:07:26 +0530

    feat: implement API data fetching

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,25 @@
 import ResCard from "./ResCard";
-import resObj from "../common/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [resList, setResList ] = useState(resObj.restaurants);
-    return (
+  const [resList, setResList ] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    
+    setResList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  };
+
+  return (
       <div>
         <button
           className="filterBtn"  
@@ -14,7 +29,7 @@ const Body = () => {
         >
           TOP RATED
         </button>
-        <div className="cardsSection" >
+        <div className="cardsSection">
           {resList.map((res) => <ResCard key={res.info.id} resData={res} />
         )}
         </div>
