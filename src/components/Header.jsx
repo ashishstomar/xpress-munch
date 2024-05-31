5af5e239d9f359937bc444496887b6d118464e1b
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-05-31 22:15:41 +0530

    feat: add toggle functionality for login/logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,28 @@
 import { LOGO_PATH } from "../common/constants";
+import { useState } from "react";
 
 const Header = () => {
+  const [loginBtn, setLoginBtn] = useState("Login")
+
     return (
       <div className="header">
         <div className="logo-wrap">
           <img className="logo" src={LOGO_PATH}/>
           <h1>XPRESSMUNCH</h1>
         </div>
-        <nav>
+        <nav className="Navbar">
           <ul>
             <li>Home</li>
             <li>Munchies</li>
             <li>Orders</li>
             <li>Cart</li>
           </ul>
+          <button 
+            className="login-btn"
+            onClick={() => loginBtn === "Login" ? setLoginBtn("Logout") :  setLoginBtn("Login")}
+          >
+            {loginBtn}
+          </button>
         </nav>
       </div>
     )
