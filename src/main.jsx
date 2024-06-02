0711751405bd1fc20d66c63ba20e3f4e681dea77
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-06-02 23:47:06 +0530

    feat: setup routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router ={appRouter}/> 
   </React.StrictMode>,
 )
