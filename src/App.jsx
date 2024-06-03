6570173e7ae54438a1ebde140effdb037f2f1b2d
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-06-03 17:46:52 +0530

    Feat: Routing with children route for client side rendering of navbar menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 import React from "react";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter, Outlet } from "react-router-dom"
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 
 const App = () => {
   return (
     <div>
         <Header />
-        <Body />
+        <Outlet />
     </div>
  )
 }
 
-export default App;
+export const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+])
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { appRouter } from './App.jsx'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import About from "./components/About";
-import Contact from "./components/Contact";
-
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  }
-])
+import { RouterProvider } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
