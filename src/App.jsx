5be51436bccdcebdd4a8578df7b2f3f8509c7433
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-06-10 00:13:10 +0530

    feat: Lazy load components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, Outlet } from "react-router-dom"
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Error from "./components/Error";
-import ResMenu from "./components/ResMenu"
+
+const Contact = lazy(() => import("./components/Contact"))
+const ResMenu = lazy(() => import("./components/ResMenu"))
+const About = lazy(() => import("./components/About"))
+const Error = lazy(() => import("./components/Error"))
+
 
 const App = () => {
   return (
@@ -27,17 +29,29 @@ export const appRouter = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <About />,
+        element: 
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <About />
+        </Suspense>,
       },
       {
         path: "/contact",
-        element: <Contact />,
+        element: 
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <Contact />
+        </Suspense>,
       },
       {
         path: "/res/:resId",
-        element: <ResMenu />
+        element: 
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <ResMenu />
+        </Suspense>,
       }
     ],
-    errorElement: <Error />,
+    errorElement: 
+    <Suspense fallback={<h1>Loading...</h1>}>
+        <Error />
+    </Suspense>,
   },
 ])
