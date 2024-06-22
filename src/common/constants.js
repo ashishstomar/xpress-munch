7de8593f5a6abc3047999aed1c07086651803439
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-06-23 00:00:28 +0530

    add: cart SVG and path

--- src/common/constants.js
+++ src/common/constants.js
@@ -3,3 +3,5 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 export const LOGO_PATH = "/src/assets/icon.svg";
 
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId="
+
+export const CART_LOGO_PATH = "/src/assets/cart.svg";
