e36950f1e00362a163315529ce401e48b23b645a
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-06-27 23:59:46 +0530

    feat: Clear cart &  btn

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { CDN_URL } from "../common/constants"
-import { deleteItem } from "../common/cartSlice";
+import { decrementItem, incrementItem, clearCart } from "../common/cartSlice";
 import { useDispatch } from "react-redux";
 
 const Cart = () => {
@@ -8,17 +8,31 @@ const Cart = () => {
     console.log(cartItem)
 
     const totalSum = cartItem.reduce((accumulator, item) => {
-        return accumulator + (item.card.info.price/100 || item.card.info.defaultPrice/100);
-    }, 0) 
+        return accumulator + (item.card.info.price/100 || item.card.info.defaultPrice/100) * item.quantity;
+    }, 0)
     
     const gst = totalSum * 18/100
+    const toPay = gst + totalSum
+    const toPayRounded = toPay.toFixed(2);
 
     console.log(totalSum)
     const dispatchAction = useDispatch()
     
+    if(cartItem.length === 0){
+        return (
+            <div className="h-screen flex justify-center items-center text-3xl font-semibold">
+                <h1 className=" bg-white px-7 py-4 text-zinc-700 rounded-full shadow-2xl">
+                    It looks empty! 🍽️ Add Something 🥟 🍧 😋  
+                </h1>
+            
+            </div>
+        );
+    }
+
     return(
         <div className="p-4 my-4">
-            <h1 className="text-3xl font-semibold text-center">cart</h1>
+            <h1 className="text-3xl font-semibold text-center">Your cart</h1>
+
             <section className="sm:mx-20 lg:mx-64 md:mx-28">
                 {cartItem.map((item) => (
                     <div 
@@ -33,31 +47,38 @@ const Cart = () => {
                             </img>
                             <span>{item.card.info.name}</span>
                         </div>
-                        <div className="flex gap-6">
+                        <div className="w-40 flex gap-6 justify-between">
                             <div className="flex text-center justify-between items-center font-semibold">
                                 <button 
                                     className="px-3 border-2 border-lime-500 bg-white text-black rounded-s-full"
-                                    onClick={() => dispatchAction(deleteItem(item))}
+                                    onClick={() => dispatchAction(decrementItem(item))}
                                 >
                                     -
                                 </button>
                                 <div
-                                    className="px-3 border-2 border-lime-500 bg-white text-black"
+                                    className=" w-8 border-2 border-l-0 border-r-0 border-lime-500 bg-white text-black"
                                 >
-                                    1
+                                    {item.quantity}
                                 </div>
                                 <button 
                                     className=" px-3 border-2 border-lime-500 bg-white text-black rounded-e-full"
-                                    onClick={() => dispatchAction(deleteItem(item))}
+                                    onClick={() => dispatchAction(incrementItem(item))}
                                 >
                                     +
                                 </button>
                             </div>
-                            <span className=" font-bold text-green-700"> ₹{item.card.info.price/100 || item.card.info.defaultPrice/100}</span>
+                            <span className=" font-bold text-green-700 text-left"> ₹{item.quantity * item.card.info.price/100 || item.card.info.defaultPrice/100}</span>
                         </div>
                     </div>
-                ))}    
-                
+                ))} 
+            <div className="flex justify-center mt-6">
+                <button 
+                    className="text-xl bg-slate-200 px-4 py-1 rounded-2xl shadow-lg"
+                    onClick={() => dispatchAction(clearCart())}
+                >
+                    Clear
+                </button>
+            </div>   
                 <section className="shadow-md flex justify-around items-center px-4 mt-10 rounded-xl bg-slate-100 py-7">
                 <button className=" shadow-xl bg-green-600 text-white font-semibold px-6 py-3 rounded-full text-lg">Checkout</button>
                     <div>
@@ -68,10 +89,9 @@ const Cart = () => {
                     <div>
                         <h3 className="font-semibold text-lg">₹{totalSum}</h3>
                         <h5 className="font-semibold">₹{gst} </h5>
-                        <h1 className="font-semibold text-xl">₹{totalSum + gst}</h1>
+                        <h1 className="font-semibold text-xl">₹{toPayRounded}</h1>
                     </div>
                 </section>
-               
             </section>
         </div>
     )
