d19f4ae543d73d6d8615c6b12a796ac6e294b23b
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-07-01 21:22:48 +0530

    feat(ui): add search bar for location selection

--- src/common/constants.js
+++ src/common/constants.js
@@ -2,6 +2,8 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 export const LOGO_PATH = "/src/assets/icon.svg";
 
+export const LPIN_PATH = "/src/assets/locationPin.svg";
+
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId="
 
 export const CART_LOGO_PATH = "/src/assets/cart.svg";
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { LOGO_PATH, CART_LOGO_PATH } from "../common/constants";
+import { LOGO_PATH, CART_LOGO_PATH, LPIN_PATH } from "../common/constants";
 import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import userContext from "../common/userContext";
@@ -6,7 +6,7 @@ import { useSelector } from "react-redux";
 
 const Header = () => {
   const [loginBtn, setLoginBtn] = useState("Login")
-
+  const [input, setInput] = useState('')
   const {signedInUser} = useContext(userContext);
 
   //selector
@@ -17,7 +17,20 @@ const Header = () => {
         <div className="flex justify-between items-center italic text-2xl font-bold">
           <img className="h-24 p-3 rounded-s-full" src={LOGO_PATH}/>
           <Link to="/"><h1>XPRESSMUNCH</h1></Link>
+          <label 
+            className="mx-12 px-3 flex bg-white rounded-3xl py-[0.3rem] "
+          >
+            <img className="h-7" src = {LPIN_PATH}/>
+            <input 
+              className="text-base w-80 px-2 font-medium text-slate-400 focus:outline-none"
+              value={input} 
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Delhi"
+            >
+            </input>
+          </label>
         </div>
+        
         <nav className="flex space-x-5 font-bold text-xl justify-center items-center mr-10 flex-wrap">
           <ul className="flex space-x-5 ">
             <li>
