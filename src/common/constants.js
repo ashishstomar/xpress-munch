20ffde4cdaf56a36d295500ef22955a27ba919b0
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-07-03 23:59:34 +0530

    add: assets burger-menu

--- src/common/constants.js
+++ src/common/constants.js
@@ -7,3 +7,7 @@ export const LPIN_PATH = "/src/assets/locationPin.svg";
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId="
 
 export const CART_LOGO_PATH = "/src/assets/cart.svg";
+
+export const BURGER_PATH = "/src/assets/burgerMenu.svg"
+
+export const CANCEL_PATH = "/src/assets/cancel.svg"
